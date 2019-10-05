3d8765efc3a7b71c36cbaaeb857a55e6c262bba6
patrickjchap <pchapman@ucdavis.edu>
2019-10-05 10:02:53 -0600

    Starting CLI implementation

--- mutation/ContractLevel/OverridingOperators.js
+++ mutation/ContractLevel/OverridingOperators.js
@@ -8,48 +8,48 @@ var utility = require('../utility/utility.js');
 
 
 let options = {
-	format: {
-		indent: {
-			style: '\t',
-			base: 0
-		},
-		newline: '\n\n',
-		space: ' ',
-		quotes: 'double'
-	}
+    format: {
+        indent: {
+            style: '\t',
+            base: 0
+        },
+        newline: '\n\n',
+        space: ' ',
+        quotes: 'double'
+    }
 };
 
 
 exports.mutateOverrideFunctionDeleteOperator = function(file, filename){
-	overridingList = utility.buildOverridingList(file);
+    overridingList = utility.buildOverridingList(file);
     console.log(overridingList);
 
 
-	fs.readFile(file, function(err, data) {	
-		if(err) throw err;
+    fs.readFile(file, function(err, data) { 
+        if(err) throw err;
 
-		fileNum = 1;
-		let mutCode = solm.edit(data.toString(), function(node) {
-			
-			if(node.type == 'FunctionDeclaration' && overridingList.indexOf(node.name) > -1) {
-				fs.writeFile("./sol_output/" + filename + "/"  
-				+ path.basename(file).slice(0, -4) + "OverriddenFunctionDelete" 
-				+ fileNum.toString() + ".sol", data.toString().replace(node.getSourceCode(),
-				""), 'ascii', function(err) {
-					if(err) throw err;
-				});
-				fileNum++;
-			}
-		});
-		
-	})
-	
+        fileNum = 1;
+        let mutCode = solm.edit(data.toString(), function(node) {
+            
+            if(node.type == 'FunctionDeclaration' && overridingList.indexOf(node.name) > -1) {
+                fs.writeFile("./sol_output/" + filename + "/"  
+                + path.basename(file).slice(0, -4) + "OverriddenFunctionDelete" 
+                + fileNum.toString() + ".sol", data.toString().replace(node.getSourceCode(),
+                ""), 'ascii', function(err) {
+                    if(err) throw err;
+                });
+                fileNum++;
+            }
+        });
+        
+    })
+    
 }
 
 exports.mutateOverrideFunctionCPC = function(file, filename) {
    overridingList = utility.buildOverridingList(file); 
 
-	fs.readFile(file, function(err, data) {
+    fs.readFile(file, function(err, data) {
         if(err) throw err;
 
         fileNum = 1;
@@ -75,7 +75,7 @@ exports.mutateOverrideFunctionCPC = function(file, filename) {
 }
 
 exports.mutateOverrideFunctionRename = function(file, filename) {
-	overridingList = utility.buildOverridingList(file);
+    overridingList = utility.buildOverridingList(file);
 
     fs.readFile(file, function(err, data) {
         if(err) throw err;
@@ -84,10 +84,10 @@ exports.mutateOverrideFunctionRename = function(file, filename) {
         let mutCode = solm.edit(data.toString(), function(node) {
 
             if(node.type == 'FunctionDeclaration' && overridingList.indexOf(node.name) > -1) {
-				//appending mutant to change the function name
-				replaceCode = node.getSourceCode().replace(node.name, node.name + "MUTANT");
-				
-				fs.writeFile("./sol_output/" + filename + "/"
+                //appending mutant to change the function name
+                replaceCode = node.getSourceCode().replace(node.name, node.name + "MUTANT");
+                
+                fs.writeFile("./sol_output/" + filename + "/"
                 + path.basename(file).slice(0, -4) + "OverridenFunctionRename"
                 + fileNum.toString() + ".sol", data.toString().replace(node.getSourceCode(),
                 replaceCode), 'ascii', function(err) {
@@ -99,4 +99,10 @@ exports.mutateOverrideFunctionRename = function(file, filename) {
 
     })
 
+}
+
+exports.mutateOverrideAll = function(file, filename) {
+    mutateOverrideFunctionDeleteOperator(file, filename);
+    mutateOverrideFunctionCPC(file, filename);
+    mutateOverrideFunctionRename(file, filename);
 }
